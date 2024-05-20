50f1f269fde254fc028ce27815cfc8bcab28eff8
frankho kevinho <frankhokevinho@yahoo.fr>
2024-05-20 07:16:19 +0100

    bugfix: do not block the execution when the redis server is down

--- services/cacheService.js
+++ services/cacheService.js
@@ -1,11 +1,21 @@
 const { redisClient } = require("../config/redisClient");
 
 const getCached = async (key) => {
-  return await redisClient.get(key);
+  try {
+    return await redisClient.get(key);
+  } catch (error) {
+    console.error(`Error to get data from redis server : ${error.message}`);
+    return null;
+  }
 };
 
 const setCached = async (key, data, expriation = -1) => {
-  await redisClient.set(key, JSON.stringify(data), "EX", expriation);
+  try {
+    await redisClient.set(key, JSON.stringify(data), "EX", expriation);
+  } catch (error) {
+    console.error(`Error to set data to redis server : ${error.message}`);
+    return false;
+  }
 };
 
 module.exports = { getCached, setCached };
